fae0b1487240aa053d73f4ed0b551c87f7013cef
Prerna Joshi <72258930+prerna250@users.noreply.github.com>
2023-06-29 19:44:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,3 @@
-// Smooth scrolling effect for anchor links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
-
-// Add fade-in animation using Animate.css
-document.querySelectorAll('.fade-in').forEach(element => {
-    element.classList.add('animate__animated', 'animate__fadeIn');
-});
-
-// Function to change the color of the heading on mouseover
-function changeHeadingColor() {
-  var heading = document.querySelector('h1');
-  heading.style.color = '#30e3cb';
-}
-
-// Function to change the color of the heading back to the original on mouseout
-function resetHeadingColor() {
-  var heading = document.querySelector('h1');
-  heading.style.color = '#66BFBF';
-}
-
-// Add event listeners to the heading for mouseover and mouseout events
-var heading = document.querySelector('h1');
-heading.addEventListener('mouseover', changeHeadingColor);
-heading.addEventListener('mouseout', resetHeadingColor);
-
-
-
-
-
-
-
-
-
 // Navbar background change on scroll
 window.addEventListener('scroll', function () {
   const navbar = document.querySelector('nav');
@@ -74,4 +34,14 @@ certificateLinks.forEach((link) => {
     const url = link.getAttribute('href');
     window.open(url, '_blank');
   });
+});
+
+// Button color change on hover
+const contactBtn = document.querySelector('.btn');
+contactBtn.addEventListener('mouseover', () => {
+  contactBtn.style.backgroundColor = '#30e3cb';
+});
+
+contactBtn.addEventListener('mouseout', () => {
+  contactBtn.style.backgroundColor = '#11cdd4';
 });
